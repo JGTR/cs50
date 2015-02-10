a691a4b5083a96420ba0429ab0a520e35313e554
Jordan <jordan.trevino@gmail.com>
2015-02-09 22:53:41 -0500

    Tweaks for styling

--- hacker1/credit.c
+++ hacker1/credit.c
@@ -2,9 +2,12 @@
 #include <stdio.h>
 #include <math.h>
 
+// The following is indented 2 spaces only out of personal preference.
+
 int longlength(long long n);
-int sumrelevantdigits(int digits, long long n);
+int sumdigits(int digits, long long n);
 int getdigit(long long n);
+bool checkfour(long long n);
 
 int main(void)
 {
@@ -12,10 +15,40 @@ int main(void)
   long long num;
   num = GetLongLong();
   int digitlength = longlength(num);
-  int relevantsum = sumrelevantdigits(digitlength, num);
-  printf("%i \n", relevantsum);
+  int relevantsum = sumdigits(digitlength, num);
+  // printf("Sum = %i \n", relevantsum);
+  bool startwfour = checkfour(num);
+  if (relevantsum % 10 == 0)
+  {
+    switch (digitlength)
+    {
+      case 13:
+        printf("VISA\n");
+        break;
+      case 16:
+        if (startwfour)
+        {
+          printf("VISA\n");
+          break;
+        }
+        else 
+        {
+          printf("MASTERCARD\n");
+          break;
+        }
+      case 15:
+        printf("AMEX\n");
+        break;
+    }
+  }
+  else
+  {
+    printf("INVALID\n");
+  }
+  
 }
 
+// Determines the length of the number.
 int longlength(long long n)
 {
   int count = 0;
@@ -24,16 +57,18 @@ int longlength(long long n)
       n /= 10 ;
       ++count;
   }
+  // printf("count: %i \n", count);
   return count;
 }
 
-int sumrelevantdigits(int d, long long longnum)
+// Sums the digits in the way they need to be summed.
+int sumdigits(int length, long long longnum)
 {
   int sum = 0;
   long long store = longnum;
   int digit;
 
-  for(int count = 0; count < d ; count++)
+  for(int count = 0; count <= length ; count++)
   {
     digit = getdigit(store);
     // printf ("digit = %i\n", digit);
@@ -41,20 +76,39 @@ int sumrelevantdigits(int d, long long longnum)
     {
       sum = sum + digit;
     }
-    else {
-      sum = sum + digit * 2;
+    else 
+    {
+      int counter = 0;
+      while(digit > 0)
+      {
+        if (counter == 0) 
+        {
+          digit = digit * 2;
+        }
+        sum = sum + getdigit(digit);
+        digit = digit / 10;
+        counter++;
+      }
     }
     store = store / 10;
     // printf("store = %llu\n", store);
   }
+
   return sum;
 }
 
 int getdigit(long long n)
 {
-  while(n > 10)
+  return n % 10;
+}
+
+bool checkfour(long long n)
+{
+  int x;
+  while(n > 0)
   {
-    n = n % 10;
+    x = n % 10;
+    n = n / 10;
   }
-  return n;
+  return x == 4;
 }

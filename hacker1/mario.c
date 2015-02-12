f77c1351b8b328bca434dafe481e8e80028b462a
Jordan <jordan.trevino@gmail.com>
2015-02-11 20:01:10 -0500

    Basic functionality for specs

--- hacker1/mario.c
+++ hacker1/mario.c
@@ -2,24 +2,53 @@
 #include <stdio.h>
 
 int renderoutcomes(int height);
-int printfoutcomes(int height);
+int printline(int spaces, int hashes);
+int printreverseline(int spaces, int hashes);
 
 int main(void)
 {
 	printf("Height: ");
 	int height = GetInt();
 	renderoutcomes(height);
-	printf("The height your provided is : %i\n", height);
-
 }
 
 int renderoutcomes(int height)
 {
-	for(int x = 0; x < height; x++)
+	for(int x = 1; x <= height; x++)
 	{
-		printf("%*s", height-x, " ");
-		printf("%*s\n",x, "#");
+		printline(height-x, x);
+		printf("  ");
+		printreverseline(height-x, x);
+		printf("\n");
 	}
 
 	return 0;
+}
+
+
+int printline(int units, int hashes)
+{
+	for(int x = 0; x < units; x++)
+	{
+		printf(" ");
+	}
+	for(int y = 0; y < hashes; y++)
+	{
+		printf("#");
+	}
+	return 0;
+}
+
+int printreverseline(int units, int hashes)
+{
+	for(int y = 0; y < hashes; y++)
+	{
+		printf("#");
+	}
+	for(int x = 0; x < units; x++)
+	{
+		printf(" ");
+	}
+	return 0;
+
 }

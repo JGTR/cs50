f8aa6bba04ae633e0792ad1dc9b12d72729bbb1e
Jordan <jordan.trevino@gmail.com>
2015-02-19 00:02:51 -0500

    Progress

--- hacker2/crack.c
+++ hacker2/crack.c
@@ -6,33 +6,69 @@
 #include <string.h>
 #define _XOPEN_SOURCE
 #include <unistd.h>
+#include <cs50.h>
 
 // Dictionary file to crack passwords
 #define PASSWORDS "/usr/share/dict/words"
 
-char getword();
+// char gettry(int counter);
+
+bool ispassword(char *try, char *pass);
 
 int main(int argc, char *argv[])
 {
+
 	FILE *tries; // file pointer for tries
 	char try[50]; // store word to try
 
 	tries = fopen(PASSWORDS, "r");
+
 	if (tries == NULL){
 		printf("There was an error loading the file.");
 	}
 
-
-	if (fgets(try, 50, tries) != NULL)
+	// Run through the entire file
+	for (int x = 1; x < 100000; x++)
 	{
-		puts(try);	
+		if (fgets(try, 50, tries) != NULL)
+		{
+			strtok(try, "\n");
+			// Stop the program once a match is reached
+			if (ispassword)
+			{
+				printf("%s is the right password. Found with %i attempts.\n", try, x);
+				break;
+			}
+		}
 	}
-	
 	return 0;
 }
 
-
-int tryword()
+bool ispassword(char *try, char *pass)
 {
-	return 1;
+	return true;
 }
+
+
+// char gettry(int counter)
+// {
+// 	FILE *tries; // file pointer for tries
+// 	char try[50]; // store word to try
+
+// 	tries = fopen(PASSWORDS, "r");
+
+// 	if (tries == NULL){
+// 		printf("There was an error loading the file.");
+// 	}
+
+// 	if (fgets(try, 50, tries) != NULL)
+// 	{
+// 		fclose(tries);
+// 		return try;
+// 	}
+// 	else 
+// 	{
+// 		return NULL;
+// 	}
+
+// }

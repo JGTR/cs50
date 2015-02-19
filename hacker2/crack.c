3d7d2ad28b753956c40212fa7e1955b03c036ee6
Jordan <jordan.trevino@gmail.com>
2015-02-18 23:29:45 -0500

    Starting on crack.c

--- hacker2/crack.c
+++ hacker2/crack.c
@@ -1,2 +1,38 @@
 // Jordan Trevino
 
+// Compilation command : clang -o crack crack.c -lcrypt
+
+#include <stdio.h>
+#include <string.h>
+#define _XOPEN_SOURCE
+#include <unistd.h>
+
+// Dictionary file to crack passwords
+#define PASSWORDS "/usr/share/dict/words"
+
+char getword();
+
+int main(int argc, char *argv[])
+{
+	FILE *tries; // file pointer for tries
+	char try[50]; // store word to try
+
+	tries = fopen(PASSWORDS, "r");
+	if (tries == NULL){
+		printf("There was an error loading the file.");
+	}
+
+
+	if (fgets(try, 50, tries) != NULL)
+	{
+		puts(try);	
+	}
+	
+	return 0;
+}
+
+
+int tryword()
+{
+	return 1;
+}

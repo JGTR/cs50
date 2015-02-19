bfbb599ae6076079a73fce7a8aff2302c2b74560
Jordan <jordan.trevino@gmail.com>
2015-02-19 01:04:34 -0500

    Not working yet

--- hacker2/crack.c
+++ hacker2/crack.c
@@ -2,9 +2,9 @@
 
 // Compilation command : clang -o crack crack.c -lcrypt
 
+#define _XOPEN_SOURCE
 #include <stdio.h>
 #include <string.h>
-#define _XOPEN_SOURCE
 #include <unistd.h>
 #include <cs50.h>
 
@@ -20,33 +20,53 @@ int main(int argc, char *argv[])
 
 	FILE *tries; // file pointer for tries
 	char try[50]; // store word to try
-
-	tries = fopen(PASSWORDS, "r");
+	tries = fopen(PASSWORDS, "r"); // Store passwords in file
+	int counter = 1; // keep count of each try
 
 	if (tries == NULL){
 		printf("There was an error loading the file.");
 	}
 
 	// Run through the entire file
-	for (int x = 1; x < 100000; x++)
+	while (fgets(try, 50, tries) != NULL)
 	{
-		if (fgets(try, 50, tries) != NULL)
+		strtok(try, "\n");
+		// Stop the program once a match is reached
+		if (ispassword(try, argv[1]))
 		{
-			strtok(try, "\n");
-			// Stop the program once a match is reached
-			if (ispassword)
-			{
-				printf("%s is the right password. Found with %i attempts.\n", try, x);
-				break;
-			}
+			printf("%s is the right password. \n", try);
+			break;
 		}
+		if (counter % 100 == 0)
+		{
+			printf("%i tries...\n", counter);
+		}
+		counter++;
 	}
 	return 0;
 }
 
 bool ispassword(char *try, char *pass)
 {
-	return true;
+	char charsforsalt[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789./";
+	char salttotry[2];
+	char *crypted;
+
+	for(int x = 0; x < 64; x++)
+	{
+		for (int y = 0; y < 64; y++)
+		{
+			salttotry[0] = charsforsalt[x];
+			salttotry[1] = charsforsalt[y];
+
+			crypted = crypt(pass, salttotry);
+			if (strcmp(crypted, try) == 0)
+			{
+				return true;
+			}
+		}
+	}
+	return false;
 }
 
 

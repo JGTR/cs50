e32465446be9e33227339431b2d51508769c5805
Jordan Trevino <jordan.trevino@gmail.com>
2015-03-25 00:27:16 -0400

    Pset 5 try

--- pset5/dictionary.c
+++ pset5/dictionary.c
@@ -8,16 +8,39 @@
  ***************************************************************************/
 
 #include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #include "dictionary.h"
+
+#define NUMOFLETTERS 27
+
+#define LINELENGTH 50
+
+#define APOSTROPHE "\'"
+
+typedef struct node
+  {
+    bool word;
+    struct node* children[NUMOFLETTERS];
+  }
+node;
+
+struct node* createnode(char newletter)
+{
+  struct node* root = malloc(sizeof(struct node));
+
+  return root;
+}
 
 /**
  * Returns true if word is in dictionary else false.
  */
 bool check(const char* word)
 {
-    // TODO
-    return false;
+  // TODO
+  return false;
 }
 
 /**
@@ -25,8 +48,52 @@ bool check(const char* word)
  */
 bool load(const char* dictionary)
 {
-    // TODO
-    return false;
+  // char *letters[sizeof(char)*NUMOFLETTERS] = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","\'"};
+  char line[sizeof(char)*LINELENGTH];
+
+  struct node *root = createnode(0);
+  // struct node *trav;
+
+  FILE *dfile;
+  dfile = fopen(dictionary, "r");
+
+  while(fgets(line, 40, dfile) != NULL)
+  {
+    node *trie = root;
+
+    int size = strlen(line);
+
+    node *alt = createnode(0);
+
+    strtok(line, "\n");
+    printf("%s", line);
+
+    for(int x = 0; x < size; x++)
+    {
+      if (strcmp(APOSTROPHE, &line[x]) == 0)
+      {
+        trie -> children[26] = alt;
+      }
+      else
+      {
+        trie -> children[25 - (97 - line[x])] = alt;
+      }
+      trie = alt;
+    }
+  }
+
+
+  return true;
+
+
+		// if (dfile != NULL)
+		// {
+		// 	return true;
+		// }
+		// else
+		// {
+		// 	return false;
+		// }   
 }
 
 /**
@@ -34,8 +101,8 @@ bool load(const char* dictionary)
  */
 unsigned int size(void)
 {
-    // TODO
-    return 0;
+  // TODO
+  return 0;
 }
 
 /**
@@ -43,6 +110,6 @@ unsigned int size(void)
  */
 bool unload(void)
 {
-    // TODO
-    return false;
+  // TODO
+  return false;
 }
